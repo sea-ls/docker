f0e28e3d90417686491c3c8ffa91ee526725c5cd
grihan181 <osagrigoriy@mail.ru>
2024-04-28 19:23:32 +0400

    add sh script 6

--- keycloak-23.0/entrypoint_overwrited.sh
+++ keycloak-23.0/entrypoint_overwrited.sh
@@ -2,3 +2,5 @@
 export $(egrep  -v '^#'  /run/secrets/* | xargs)
 # if you need some specific file, where password is the secret name
 # export $(egrep  -v '^#'  /run/secrets/password| xargs)
+# call the dockerfile's entrypoint
+source /docker-entrypoint.sh
