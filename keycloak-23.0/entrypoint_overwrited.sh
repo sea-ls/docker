4e6085970fdf87b2358b72a6837a4aaa5acbb1f1
grihan181 <osagrigoriy@mail.ru>
2024-04-29 01:50:04 +0400

    влхзварощение к истокам

--- keycloak-23.0/entrypoint_overwrited.sh
+++ keycloak-23.0/entrypoint_overwrited.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+touch  ~/.profile
 for i in $(ls -1 /run/secrets)
 do
+  echo ${i}
+  echo cat /run/secrets/${i}
+  echo "export ${i}=$(cat /run/secrets/${i})" >> ~/.profile
 	export ${i}=$(cat /run/secrets/${i})
 done
 
-"$@"
+source ~/.profile
+exec "$@"
