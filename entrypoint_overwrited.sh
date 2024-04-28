8c98b264bafd02fd827cf263c04bd1856d92c55c
grihan181 <osagrigoriy@mail.ru>
2024-04-28 19:04:02 +0400

    add sh script

---
+++ entrypoint_overwrited.sh
@@ -0,0 +1,4 @@
+# get your envs files and export envars
+export $(egrep  -v '^#'  /run/secrets/* | xargs)
+# if you need some specific file, where password is the secret name
+# export $(egrep  -v '^#'  /run/secrets/password| xargs)
